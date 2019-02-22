51600459b851b7766f4729bf4e17e23401f8f261
Robbie Averill <robbie@silverstripe.com>
2019-02-22 15:21:08 +0700

    API SubsiteSecurityReport is now deprecated, please use YAML configuration instead

--- src/Subsites/SubsiteSecurityReport.php
+++ src/Subsites/SubsiteSecurityReport.php
@@ -7,6 +7,7 @@ use SilverStripe\Core\Extension;
 /**
  * User Security Report extension for Subsites
  *
+ * @deprecated 2.1.0 This extension will be removed in 3.0, configuration should be put in YAML instead
  * @author Damian Mooyman <damian@silverstripe.com>
  */
 class SubsiteSecurityReport extends Extension
